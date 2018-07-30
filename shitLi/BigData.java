15ac39b514bd9be2392b8f5cda5208db04874850
yangziyu1230 <yangziyu1230@live.com>
2018-07-30 21:38:27 +0800

    update

--- shitLi/BigData.java
+++ shitLi/BigData.java
@@ -1,85 +1,95 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class BigData {
 
     public static void main(String[] args) {
-        String inFilePath = "/home/yang/Desktop/13301104001";
+        String inFolderPath = "/home/yang/Desktop/zzz";//你存放文件的目录(zzz下含有此格式所有文件)
         String encoding = "UTF8";
-        String outFilePath = "/home/yang/Desktop/out";
-        File inFile;
+        String outFilePath = "/home/yang/Desktop/out";//你要输出到的文件(out是一个文件，不是一个文件夹，也就是说艘有数据输出到同一个文件里)
+        ArrayList<String> files = new ArrayList<>();
+
+        File inFolder;
         File outFile;
         InputStreamReader in = null;
         BufferedReader buff = null;
         OutputStream out = null;
 
-        inFile = new File(inFilePath);
+        inFolder = new File(inFolderPath);
         outFile = new File(outFilePath);
 
-        try {
-            in = new InputStreamReader(new FileInputStream(inFile), encoding);
-            buff = new BufferedReader(in);
-            out = new FileOutputStream(outFile);
-            if (inFile.isFile() && inFile.exists()) {
+        File[] inFiles = inFolder.listFiles();
 
-                String line1 = buff.readLine();
-                String line2 = buff.readLine();
+        for (int i = 0; i < inFiles.length; i++) {
+            try {
+                in = new InputStreamReader(new FileInputStream(inFiles[i]), encoding);
+                buff = new BufferedReader(in);
+                out = new FileOutputStream(outFile, true);
 
-                while (line1 != null && line2 != null) {
-                    boolean bool = true;//判断上一个是否输出了两个
+                if (inFiles[i].isFile() && inFiles[i].exists()) {
 
-                    String words1[] = line1.split(",");
-                    String words2[] = line2.split(",");
+                    boolean bool = false;//判断上一个是否输出了两个
 
-                    //转化为Double
-                    double num11 = Double.parseDouble(words1[4]);
-                    double num12 = Double.parseDouble(words1[5]);
-                    double num21 = Double.parseDouble(words2[4]);
-                    double num22 = Double.parseDouble(words2[5]);
+                    String line1 = buff.readLine();
+                    String line2 = buff.readLine();
 
-                    if (Integer.parseInt(words1[8]) <= 90) {
-                        if (getDistance(num11, num12, num21, num22) < 2000) {//小于2000全写
-                            if (bool) { //如果上一次输出了两行，此次再输出line1就重复了（这一行的line1等于上一次的line2）
-                                byte data[] = (line2 + "\n").getBytes();
-                                out.write(data);
-                            } else {
-                                byte data[] = (line1 + "\n").getBytes();
-                                out.write(data);
-                                data = (line2 + "\n").getBytes();
-                                out.write(data);
+                    while (line1 != null && line2 != null) {
+
+
+                        String words1[] = line1.split(",");
+                        String words2[] = line2.split(",");
+
+                        //转化为Double
+                        double num11 = Double.parseDouble(words1[4]);
+                        double num12 = Double.parseDouble(words1[5]);
+                        double num21 = Double.parseDouble(words2[4]);
+                        double num22 = Double.parseDouble(words2[5]);
+
+                        if (Integer.parseInt(words1[8]) <= 90) {
+                            if (getDistance(num11, num12, num21, num22) < 2000) {//小于2000全写
+                                if (bool) { //如果上一次输出了两行，此次再输出line1就重复了（这一行的line1等于上一次的line2）
+                                    byte data[] = (line2 + "\n").getBytes();
+                                    out.write(data);
+                                } else {
+                                    byte data[] = (line1 + "\n").getBytes();
+                                    out.write(data);
+                                    data = (line2 + "\n").getBytes();
+                                    out.write(data);
+                                }
+                                bool = true;
+                            } else if (getDistance(num11, num12, num21, num22) >= 2000) {//大于2000只写第一个
+                                if (!bool) { //和上面同理
+                                    byte data[] = (line1 + "\n").getBytes();
+                                    out.write(data);
+                                }
+                                bool = false;
                             }
-                            bool = true;
-                        } else if (getDistance(num11, num12, num21, num22) >= 2000) {//大于2000只写第一个
-                            if (!bool) { //和上面同理
-                                byte data[] = (line1 + "\n").getBytes();
-                                out.write(data);
-                            }
+
+                        } else {
                             bool = false;
                         }
 
-                    } else {
-                        bool = false;
+                        //递进
+                        line1 = line2;
+                        line2 = buff.readLine();
                     }
-
-                    //递进
-                    line1 = line2;
-                    line2 = buff.readLine();
                 }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            in.close();
-            buff.close();
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+            try {
+                in.close();
+                buff.close();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
