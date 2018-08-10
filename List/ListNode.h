c8e22900fc331f1e3162a3d3904686fbd1c2049f
unknown <yangziyu1230@live.com>
2018-08-10 22:57:50 +0800

    update

--- List/ListNode.h
+++ List/ListNode.h
@@ -1,4 +1,4 @@
-//
+﻿//
 // Created by yang on 18-8-2.
 //
 
@@ -8,18 +8,32 @@
 typedef int Rank;
 #define ListNodePosi(T) ListNode<T>*
 
-template<typename T>
+template<class T>
 struct ListNode
 {
-    T data;
-    ListNodePosi(T) pred;
-    ListNodePosi(T) succ;
+	T data;
+	ListNodePosi(T) pred;
+	ListNodePosi(T) succ;
 
-    ListNode() {}
-    ListNode(T e, ListNodePosi(T)p = nullptr, ListNodePosi(T)s = nullptr)
-            : data(e), pred(p), succ(s) {}
-    ListNodePosi(T) insertAsPred(const T &e);
-    ListNodePosi(T) insertAsSucc(const T &e);
+	ListNode() {}
+	ListNode(T e, ListNodePosi(T)p = nullptr, ListNodePosi(T)s = nullptr)
+		: data(e), pred(p), succ(s) {}
+	
+	ListNodePosi(T) insertAsPred(const T &e)
+	{
+		ListNodePosi(T) x = new ListNode(e, pred, this);
+		pred->succ = x;
+		pred = x;
+		return x;
+	}
+	
+	ListNodePosi(T) insertAsSucc(const T &e)
+	{
+		ListNodePosi(T) x = new ListNode(e, this, succ);
+		succ->pred = x;
+		succ = x;
+		return x;
+	}
 
 };
 
--- List/main.cpp
+++ List/main.cpp
@@ -1,14 +1,12 @@
 ﻿#include <iostream>
-#include "Vector.h"
+#include "List.h"
 
 using namespace std;
 
-
-
 int main(int argc, char* argv[])
 {
-	Vector<int> v;
-	v.insert(1);
+	List<int> v;
+	v.insertAsFirst(1);
 	cout << v[0] << endl;
 	system("pause");
 	return 0;
